5b528bf0000d25bc7a22dd816c4c307d57e6a478
OmarCharlesGedeon <charles@effortlesshost.com>
2019-01-04 10:43:31 -0800

    Completed Sql tables and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Moes Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('4x4 with Animal Style', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chic Filla Chicken Sandwich', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Fried Steak Mushroom Burger', true);
